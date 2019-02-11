cb7a4e61615d9828c2643f33e1b59020d7424f44
Chirag Mahaveer Parmar <chiragmahaveerparmar@Chirags-MBP.lan>
2019-02-11 10:07:28 +0100

    clean up

--- src/debug.ts
+++ src/debug.ts
@@ -1,6 +1,22 @@
+// Copyright 2018 Energy Web Foundation
+// This file is part of the Origin Application brought to you by the Energy Web Foundation,
+// a global non-profit organization focused on accelerating blockchain technology across the energy sector,
+// incorporated in Zug, Switzerland.
+//
+// The Origin Application is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY and without an implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details, at <http://www.gnu.org/licenses/>.
+//
+// @authors: slock.it GmbH; Heiko Burkhardt, heiko.burkhardt@slock.it; Martin Kuechler, martin.kuchler@slock.it
+
 import * as Treeify from 'treeify'
 import * as Colors from 'colors/safe'
-import {PreciseProofs} from './' 
+import {PreciseProofs} from './'
 
 export const printMerkleTree = (merkleTree: any[], leafs: PreciseProofs.Leaf[], schema?: string[]) => {
 
@@ -8,13 +24,13 @@ export const printMerkleTree = (merkleTree: any[], leafs: PreciseProofs.Leaf[], 
 
     let row
     let lowerLevel = merkleTree[0]
-    let remainder = [] 
+    let remainder = []
     const merkleTreeRootHash = merkleTree[merkleTree.length-1][0]
 
     for(let i = 1; i < merkleTree.length; i++ ) {
 
         row = merkleTree[i]
-    
+
 
         lowerLevel = i != 1 && lowerLevel.length % 2 !== 0 ? merkleTree[i-1].concat(lowerLevel[lowerLevel.length - 1]) : merkleTree[i-1]
 
@@ -27,22 +43,22 @@ export const printMerkleTree = (merkleTree: any[], leafs: PreciseProofs.Leaf[], 
             const right = lowerLevel[j * 2 + 1]
 
             let newNode = {
-                
+
                 [lowerLevel[j * 2]]: i === 1 ?
-                    null : 
+                    null :
                     remainder[left] ?
                         remainder[left] :
                         {...resultObject[left]},
-                [lowerLevel[j * 2 + 1]]: i === 1 ? 
+                [lowerLevel[j * 2 + 1]]: i === 1 ?
                     null :
                     remainder[right] ?
                         remainder[right] :
                         {...resultObject[right]}
             }
             remainder[hash] = newNode
-        
+
             line[hash] = newNode
-            
+
         }
 
         resultObject = line
@@ -51,33 +67,33 @@ export const printMerkleTree = (merkleTree: any[], leafs: PreciseProofs.Leaf[], 
 
     const schemaHash = schema ? PreciseProofs.hashSchema(schema) : null
 
-    
 
-    resultObject = schema ? { 
+
+    resultObject = schema ? {
         [PreciseProofs.createExtendedTreeRootHash(merkleTreeRootHash, schema)]: {
             ...resultObject,
             [schemaHash]: null
-        } 
+        }
     } : resultObject
-        
+
     let leafCounter = 0
 
     console.log('\n- Tree View\n')
     Treeify.asLines(resultObject, true, (data: string) => {
         const hashLength = 64
         if (leafs) {
-            
-            
+
+
             const leadingSpaces = data.length - data.trim().length
-            
-            const leaf = leafs.find((leaf: PreciseProofs.Leaf) => 
+
+            const leaf = leafs.find((leaf: PreciseProofs.Leaf) =>
                 leaf.hash === data.substring(data.length - hashLength, data.length)
             )
             const theHash = data.substring(data.length - hashLength, data.length)
             const shortHash = theHash.substring(0, 4) + '...' + theHash.substring(theHash.length - 4, theHash.length)
-            
-            
-            
+
+
+
             if (leadingSpaces === 0) {
                 console.log(
                     data.substring(0, data.length - hashLength ) +
@@ -94,9 +110,9 @@ export const printMerkleTree = (merkleTree: any[], leafs: PreciseProofs.Leaf[], 
                     Colors.underline(Colors.green(shortHash)) + ' ' +
                     Colors.green(`[${leafCounter++}] `) +
                     Colors.grey(leaf.key + ': ' + leaf.value + ' (salt: ' + leaf.salt + ')')
-                ) 
+                )
             } else if (theHash === schemaHash) {
-                
+
                 console.log(
                     data.substring(0, data.length - hashLength) +
                     Colors.underline(Colors.blue(shortHash)) + Colors.blue(' Schema'))
@@ -104,9 +120,9 @@ export const printMerkleTree = (merkleTree: any[], leafs: PreciseProofs.Leaf[], 
                 schema.forEach((element: string, index: number) => {
                     console.log(Colors.grey(`      [${index}] ${element}`))
                 })
-                console.log() 
+                console.log()
             } else {
-          
+
                 console.log(
                     data.substring(0, data.length - hashLength) +
                     Colors.underline(shortHash))
@@ -117,8 +133,8 @@ export const printMerkleTree = (merkleTree: any[], leafs: PreciseProofs.Leaf[], 
                 data.substring(0, data.length - hashLength) +
                 data.substring(data.length - hashLength, data.length))
         }
-        
-        
+
+
     })
 
 }
